55f0b0a56452b50e6e8e0d9b71a24dc0701f87dd
Jaschar Domann <Jaschar.Domann@dai-labor.de>
2013-10-14 18:32:02 +0200

    add a JUbit test for loadResourceAsNioPath

--- src/test/java/alcar/commons/resource/ResourceLoaderUnit.java
+++ src/test/java/alcar/commons/resource/ResourceLoaderUnit.java
@@ -3,6 +3,9 @@ package alcar.commons.resource;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.Test;
 
@@ -13,9 +16,15 @@ public class ResourceLoaderUnit {
 	}
 	
 	@Test
-	public void loadResourceAsFile() {
+	public void loadResourceAsFileUnit() {
 		File file = ResourceLoader.loadResourceAsFile("/test/test.txt");
 		assertTrue(file.exists());
 		assertTrue(file.isFile());
+	}
+
+	@Test
+	public void loadResourceAsNioPathUnit() throws IOException {
+		Path nioPath = ResourceLoader.loadResourceAsNioFilePath("/test/test.txt");
+		assertTrue(Files.exists(nioPath));
 	}
 }
